f6fea44b5b15639cd5c0d3961ed7002308ec3110
vn536zl <haywardj@bentonvillek12.org>
2022-08-30 09:26:54 -0500

    Init comit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+import pieces.gamePiece
+
+def main():
+    print("running...")
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ pieces/gamePiece.py
@@ -0,0 +1,7 @@
+import imp
+
+
+def __init__(self, image, color, position, ):
+    self.image = image
+    self.color = color
+    self.position = position
