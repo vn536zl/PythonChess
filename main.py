e17999c9591ec164f1c9cc90d2b73ad1bd3ad9e5
vn536zl <haywardj@bentonvillek12.org>
2022-08-30 12:09:39 -0500

    Start of gamepiece class

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
+from math import *
 import pygame
 import pieces.gamePiece
 
+black, white = (0, 0, 0), (255, 255, 255)
+screen_size = [350, 350]
+width = screen_size[0] / 8
+height = screen_size[1] / 8
+
+def loop(src):
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouseDown()
+        pygame.display.flip()
+
+def drawBoard(src):
+    color_pic = 0
+    for y in range(8):
+        for x in range(8):
+            if color_pic == 0:
+                color = white
+            elif color_pic == 1:
+                color = black
+            pygame.draw.rect(src, color, [width * x, height * y, width, height])
+            color_pic = 0 if color_pic == 1 else 1
+        color_pic += 1
+            
+
+def mouseDown():
+    mouse_pos = pygame.mouse.get_pos()
+    print("Mouse x: ", floor(mouse_pos[0] / width) + 1, " Mouse y: ", floor(mouse_pos[1] / height) + 1)
+
 def main():
+    pygame.init()
+    src = pygame.display.set_mode(screen_size)
+    pygame.display.set_caption('Chess')
+    clock = pygame.time.Clock()
+    clock.tick(50)
+    drawBoard(src)
     print("running...")
+    loop(src)
+    pygame.quit()
+    
     
     
 if __name__ == "__main__":
--- pieces/gamePiece.py
+++ pieces/gamePiece.py
@@ -1,7 +1,23 @@
-import imp
+import pygame
 
+class gamePiece(pygame.sprite.Sprite):
 
-def __init__(self, image, color, position, ):
-    self.image = image
-    self.color = color
-    self.position = position
+    def __init__(self, image, color, position, screenSize, visible):
+        super().__init__(self)
+        
+        self.image = pygame.image.load(image)
+        self.color = color
+        self.position = position
+        self.screenSize = screenSize
+        self.width = screenSize(1) / 8
+        self.height = screenSize(0) / 8
+        self.visible = visible
+        
+    def drawPiece(self, src, position =None):
+        
+        if position == None:
+            position = self.position
+        
+        if self.visible:
+            self.rect = pygame.Rect((position(0) * self.width, position(1) * self.height), self.width, self.height)
+            self
